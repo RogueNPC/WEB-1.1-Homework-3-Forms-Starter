bdbce360e22c16863ae45b8e6df75215eeebe62c
RogueNPC <jordanbtansay@gmail.com>
2020-11-11 16:43:52 -0800

    added compliments

--- app.py
+++ app.py
@@ -51,8 +51,14 @@ def compliments():
 @app.route('/compliments_results')
 def compliments_results():
     """Show the user some compliments."""
+    num_compliments = request.args.get('num_compliments')
+    rand_compliments = random.sample(list_of_compliments, int(num_compliments))
+
     context = {
         # TODO: Enter your context variables here.
+        'users_name': request.args.get('users_name'),
+        'wants_compliments': request.args.get('wants_compliments'),
+        'rand_compliments': rand_compliments
     }
 
     return render_template('compliments_results.html', **context)
